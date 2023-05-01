d188847c98133cd0b8ea372a1ac2f76ab19884fa
Jeevithakavidevan <132261301+Jeevithakavidevan@users.noreply.github.com>
2023-05-01 22:07:04 +0530

    Update streamit_agas.py

--- streamit_agas.py
+++ streamit_agas.py
@@ -1,7 +1,7 @@
 import pandas
 import streamlit
 streamlit.text("Hi Kitchen people")
-
+streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
