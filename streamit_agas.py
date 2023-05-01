1d94e7e933c5152010e105fb8c244c8179c8847e
Jeevithakavidevan <132261301+Jeevithakavidevan@users.noreply.github.com>
2023-05-01 16:13:15 +0530

    Update streamit_agas.py

--- streamit_agas.py
+++ streamit_agas.py
@@ -1,3 +1,5 @@
 import pandas
+import streamlit
+streamlit.text("Hi Kitchen people")
 my_fru_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fru_list)
