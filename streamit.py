33551c24d92ad0c6418172dcde1933be1b7dd5f7
Jeevithakavidevan <132261301+Jeevithakavidevan@users.noreply.github.com>
2023-05-01 16:05:02 +0530

    Update streamit.py

--- streamit.py
+++ streamit.py
@@ -1,5 +1,3 @@
-import streamlit
-streamlit.title('🥣 Hi Kitchen dinner people')
-streamlit.header('🥑 Breadkfast menu')
-streamlit.title('🥗 Omega 3 fatty acid')
-streamlit.title('🐔🍞 black kberris')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
