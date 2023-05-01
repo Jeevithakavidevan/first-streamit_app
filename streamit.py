3e3748b4990cf5f07dba48b4941044e265ccd146
Jeevithakavidevan <132261301+Jeevithakavidevan@users.noreply.github.com>
2023-05-01 16:00:38 +0530

    Update streamit.py
    fix code

--- streamit.py
+++ streamit.py
@@ -1,2 +1,5 @@
 import streamlit
-streamlit.title('Hi Lord shiva ')
+streamlit.title('Hi Kitchen dinner people')
+streamlit.header('Breadkfast menu')
+streamlit.title('Omega 3 fatty acid')
+streamlit.title('black kberris')
